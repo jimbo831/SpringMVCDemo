913986d6fac8b2cb967e1f612f87bcc0bb8a573c
jimbo831 <jimbo831@gmail.com>
2016-11-20 01:05:34 -0600

    Pulling drop-down selection options from the student object instead of hard coded into the JSP file

--- src/main/java/com/johnbwhitejr/springdemo/mvc/beans/Student.java
+++ src/main/java/com/johnbwhitejr/springdemo/mvc/beans/Student.java
@@ -1,5 +1,7 @@
 package com.johnbwhitejr.springdemo.mvc.beans;
 
+import java.util.LinkedHashMap;
+
 /**
  * Created by johnwhite on 11/20/16.
  */
@@ -9,6 +11,17 @@ public class Student {
     private String firstName;
     private String lastName;
     private String country;
+
+    private LinkedHashMap<String, String> countryOptions;
+
+    public Student() {
+        countryOptions = new LinkedHashMap();
+        countryOptions.put("US", "United States");
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+    }
 
     public String getFirstName() {
         return firstName;
@@ -32,5 +45,9 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
     }
 }
